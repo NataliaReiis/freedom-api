3b5e0a95137f004cd30af2b4acb9f381929e7b12
NataliaReiis <nataliareis208@gmail.com>
2024-06-16 17:49:24 -0300

    feat: server run

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import fastify from "fastify";
-import fastifyCors from "@fastify/cors";
-import dotenv from "dotenv";
-
-const app = fastify();
-
-app.listen({ port: 3333 }).then(() => {
-  console.log("Servidor rodando na porta 3333");
-});

ee26b15361441e230ff75fb8e6ea6552b2d537ce
NataliaReiis <nataliareis208@gmail.com>
2024-06-25 23:39:30 -0300

    feat: include postBlog interface

--- src/interfaces/postBlog.interface.ts
+++ src/interfaces/postBlog.interface.ts
@@ -1,5 +1,29 @@
-export interface PostBlog {
+import { Comment } from './comment.interface'
+
+export type PostBlog = {
   id: string
   description: string
   imagePost: string
+  userId: string
+  comment?: Comment[]
+}
+
+export type PostBlogCreate = {
+  description: string
+  imagePost: string
+  userId: string
+}
+
+export type PostBlogUpdated = {
+  userId: string
+  description?: string
+  imagePost?: string
+}
+
+export interface PostBlogRepository {
+  create(data: PostBlogCreate): Promise<PostBlog>
+  findAll(): Promise<PostBlog[]>
+  findById(id: string): Promise<PostBlog | null>
+  update(id: string, data: Partial<PostBlogUpdated>): Promise<PostBlog>
+  delete(id: string): Promise<PostBlog>
 }

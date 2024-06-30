87354a327d48044c61b50dfab9ee23947c539a9c
NataliaReiis <nataliareis208@gmail.com>
2024-06-29 22:21:53 -0300

    feat: include prisma create in complaint repository

--- src/usecases/complaint.usecase.ts
+++ src/usecases/complaint.usecase.ts
@@ -17,4 +17,31 @@ class UseCaseComplaint {
 
     return result
   }
+
+  async getAll(): Promise<Complaint[]> {
+    return await this.complaintRepository.findAll()
+  }
+
+  async getId(id: string): Promise<Complaint | null> {
+    return await this.complaintRepository.findById(id)
+  }
+
+  async update(id: string, data: UpdatedComplaint): Promise<Complaint | null> {
+    const complaintExists = await this.complaintRepository.findById(id)
+    if (!complaintExists) {
+      throw new Error('Complaint not found')
+    }
+    const updatedComplaint = await this.complaintRepository.update(id, data)
+    return updatedComplaint
+  }
+
+  async delete(id: string): Promise<void> {
+    const complaintExists = await this.complaintRepository.findById(id)
+    if (!complaintExists) {
+      throw new Error('Complaint not found')
+    }
+    await this.complaintRepository.delete(id)
+  }
 }
+
+export { UseCaseComplaint }

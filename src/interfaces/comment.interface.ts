d3e25e82059e0e1f91762828c16a70054931fe40
NataliaReiis <nataliareis208@gmail.com>
2024-07-02 19:02:37 -0300

    feat: include comments interface

--- src/interfaces/comment.interface.ts
+++ src/interfaces/comment.interface.ts
@@ -1,17 +1,25 @@
-export interface Comment {
+export type CommentBase = {
   id: string
   description: string
   imageComment: string
   createdAt: Date
   updatedAt: Date
+  userId: string
 }
 
-export interface CreateComment {
-  description: string
-  imageComment: string
-}
+export type Comment = Omit<CommentBase, 'userId'>
 
-export interface UpdatedComment {
-  description?: string
-  imageComment?: string
+export type CreateComment = Pick<
+  CommentBase,
+  'description' | 'imageComment' | 'userId'
+>
+
+export type UpdatedComment = Partial<CommentBase>
+
+export interface CommentRepository {
+  create(data: CreateComment): Promise<Comment>
+  findById(id: string): Promise<Comment | null>
+  findAll(): Promise<Comment[]>
+  update(id: string, data: UpdatedComment): Promise<Comment | null>
+  delete(id: string): Promise<void>
 }

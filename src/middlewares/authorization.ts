42e84aa6a1c8b600283941864c0434e7d588cf19
NataliaReiis <nataliareis208@gmail.com>
2024-09-16 20:30:43 -0300

    feat: cria middlewate de autorização

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,36 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import env from 'dotenv'
+
+env.config()
+
+export default async function authMiddleware(
+  req: FastifyRequest,
+  reply: FastifyReply
+): Promise<void> {
+  const jwt_secret = process.env.JWT_SECRET
+
+  if (!jwt_secret)
+    return reply.send('JWT_SECRET não está definido nas variáveis de ambiente')
+  try {
+    const auth = req.headers.authorization ?? ''
+    if (!auth) {
+      reply.status(401).send('Você não tem permissão para essa ação')
+    }
+
+    const token = auth.split(' ')[1]
+
+    if (!token) {
+      reply.status(401).send('Token não fornecido')
+      return
+    }
+    jwt.verify(token, jwt_secret, (err, decoded) => {
+      if (err) {
+        reply.status(401).send('Token invalido ou expirado')
+      }
+      req.email = (decoded as JwtPayload).email
+    })
+  } catch (err) {
+    reply.status(500).send('Erro interno do servidor')
+  }
+}

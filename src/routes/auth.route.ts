c70669a7aabc9ec2f02141fb63693a2c25ef8a3b
NataliaReiis <nataliareis208@gmail.com>
2024-09-16 20:31:11 -0300

    feat: cria rota de autenticação

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { UserUseCase } from '../usecases/user.usecase'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import env from 'dotenv'
+
 interface Auth {
   email: string
   password: string
@@ -8,15 +11,34 @@ interface Auth {
 export async function auth(fastify: FastifyInstance) {
   const userUseCase = new UserUseCase()
 
+  env.config()
+
   fastify.post<{ Body: Auth }>('/', async (req, reply) => {
     const { email, password } = req.body
     const user = await userUseCase.findByEmail(email)
+    const jwt_secret = process.env.JWT_SECRET
+
     if (!user || !password) {
-      reply.status(400).send('Credencias invalidas')
-    } else if (bcryptjs.compareSync(password, user.password)) {
-      reply.send('Usuário autenticado')
-    } else {
-      reply.status(400).send('Credencias inválidas')
+      return reply.status(400).send('Credencias invalidas')
+    }
+
+    if (!jwt_secret)
+      return reply.send(
+        'JWT_SECRET não está definido nas variáveis de ambiente'
+      )
+    try {
+      const passwordMatches = bcryptjs.compareSync(password, user.password)
+
+      if (passwordMatches) {
+        const token = jwt.sign({ email }, jwt_secret, {
+          expiresIn: 3600,
+        })
+        reply.send({ email, token })
+      } else {
+        return reply.status(400).send('Credencias invalidas')
+      }
+    } catch (err) {
+      return reply.status(400).send('Credencias invalidas')
     }
   })
 }

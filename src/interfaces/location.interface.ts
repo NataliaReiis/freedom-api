29889a273d28745613db4256f2fd6c60fc6fa8ea
NataliaReiis <nataliareis208@gmail.com>
2024-10-24 23:24:22 -0300

    refactor: code improvement from location

--- src/interfaces/location.interface.ts
+++ src/interfaces/location.interface.ts
@@ -1,16 +1,10 @@
-export type LocationBase = {
-  id: string
+export type CreateLocationDto = {
   latitude: number
   longitude: number
-  createdAt: Date
 }
 
-export type Location = Omit<LocationBase, 'id' | 'createdAt'>
+export type UpdateLocationDto = Partial<CreateLocationDto>
 
-export type CreateLocation = Pick<LocationBase, 'latitude' | 'longitude'>
-
-export interface LocationRepository {
-  create(data: CreateLocation): Promise<Location>
-  findAll(): Promise<Location[]>
-  findById(id: string): Promise<Location | null>
+export type GetLocationSearchParams = {
+  id: string
 }

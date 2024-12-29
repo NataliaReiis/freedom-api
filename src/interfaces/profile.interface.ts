385793fe1547e524420bd2d5071fc93b0b843118
NataliaReiis <nataliareis208@gmail.com>
2024-12-29 01:04:24 -0300

    feat: add profile interface

---
+++ src/interfaces/profile.interface.ts
@@ -0,0 +1,14 @@
+export type Profile = {
+  id: string
+  name: string
+  tel: string
+  age: number
+  sex: string
+  cpf: string
+  marital_status: string
+  userID: string
+}
+
+export type CreateProfile = Omit<Profile, 'id'>
+
+export type UpdateProfile = Partial<CreateProfile>
